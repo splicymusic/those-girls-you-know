2dc33e7abf5b7e044b66eeef77631042d7d16ba3
Brian Risk <brian@sift.co>
2020-04-14 00:17:40 -0400

    undulating waves in 2nd chorus

---
+++ js/actors/Wave.js
@@ -0,0 +1,92 @@
+import * as THREE from "../../lib/three/build/three.module.js";
+import * as Utils from "../Utils.js";
+import Actor from "./Actor.js";
+
+
+class Wave extends Actor {
+
+    constructor(scene, clock, loader) {
+        super(scene, clock, loader);
+        let paths = [];
+        loader.imageList.forEach(image => {
+            if (image.includes("girls") && !image.endsWith(".png")) {
+                paths.push(image);
+            }
+        });
+
+        let xLoc = Utils.locationInSong(0, 30, 0);
+        this.amplitude = 0;
+        let arenaSize = 50;
+        let canyonWidth = 2;
+        let cardWidth = 2;
+        let leftBound = -canyonWidth;
+        let rightBound = canyonWidth;
+
+        let buildings = [];
+        let group = new THREE.Group();
+
+        let xMin = xLoc;
+        let xMax = xLoc + arenaSize;
+        let yMin = arenaSize / -2;
+        let yMax = arenaSize / 2;
+        let xPos = xMin;
+        let spacing = cardWidth;
+        let count = 0;
+        while (xPos <= xMax) {
+            let yPos = yMin;
+            while (yPos <= yMax ) {
+
+                // if (yPos > leftBound && yPos < rightBound) {
+                //     yPos += spacing;
+                //     continue;
+                // }
+                let path = paths[count % paths.length];
+                let building = loader.getPlane(path, cardWidth);
+
+                building.position.x = xPos + Math.random() * 2;
+                building.position.y = yPos;
+                building.position.z = -2;
+                buildings.push(building);
+                group.add(building);
+                yPos += spacing;
+                count++;
+            }
+            xPos += spacing;
+        }
+        console.log("pics created: " + count);
+
+
+
+        this.buildings = buildings;
+
+        // group.visible = false;
+        this.group = group;
+        scene.add(group);
+    }
+
+
+    update(cameraPosition, fpsAdjustment) {
+        if (cameraPosition > Utils.locationInSong(0, 29.25, 0) - 0.1) {
+            if (this.amplitude < 1) {
+                this.amplitude += .01 * fpsAdjustment;
+            } else {
+                this.amplitude = 1;
+            }
+
+            this.buildings.forEach(building => {
+                // amplitude will vary sinusoidally
+                let ampMultiplier = Math.cos(this.clock.quarterFraction * Math.PI / 2) * Math.sqrt(4);
+                ampMultiplier *= ampMultiplier;
+                // building.position.z = Math.sin((building.position.y + this.clock.eighthsFraction) * Math.PI / 2) * ampMultiplier - 4 + ampMultiplier;
+                let xFactor =  Math.sin((building.position.x - cameraPosition) * Math.PI / 16);
+                let yFactor =  Math.sin((building.position.y - cameraPosition) * Math.PI / 16);
+                building.position.z = xFactor * yFactor * 4 * this.amplitude -2  ;
+            });
+        }
+
+
+    }
+
+}
+
+export default Wave;
